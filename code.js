b5a2c07368aafec1e3a0fb4bc09e7cf8ae467ce9
bastiaan hersman <bastiaan.hers@gmail.com>
2018-02-06 11:21:36 +0100

    basic styling and start buttom

---
+++ code.js
@@ -0,0 +1,26 @@
+
+
+
+
+function clicked(data){
+    switch(data) {
+        case "start":
+            console.log("clicked start");
+            document.getElementById("title").innerHTML = subjects[0].title ;
+            document.getElementById("statement").innerHTML =  subjects[0].statement;
+        break;
+        case "eens":
+    
+        break;       
+        case "oneens":
+    
+        break;
+        case "geenVanBeide":
+        
+        break;       
+        case "skip":
+        
+        break;
+    }
+
+}
