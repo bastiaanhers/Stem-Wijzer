d1b6067a6090709e7747cf854773cbc883cf01d2
bastiaan hersman <bastiaan.hers@gmail.com>
2018-02-13 13:05:39 +0100

    changed the way pages are build and navigated

--- code.js
+++ code.js
@@ -1,58 +1,68 @@
-var page = document.getElementById("main");
-var pageCount = 0;
-var options = ["eens","geenVanBeide", "oneens",  "skip"];
+var pageNumb = -1;
+//start page element
+var page = document.getElementById("page");
+//statement page element
+var statementPage = document.getElementById("statementPage"); 
+//statement elements
+var titleText = document.getElementById("titleText");
+var statementText = document.getElementById("statementText");
+//result page
+var resultsPage = document.getElementById("resultsPage");
+
 var results = [];
 
 
-function clearPage(){
-    page.innerHTML = "";
-}
-function buildSkipped(){
-    clearPage();
-    skippedShow();
-
-
-}
-
-function buildResultPage(){
-    clearPage();
-    
-
-}
-
-function buildStatement(pageNumb){
-    clearPage();
-    if(pageCount >= 1){
-        backButtonShow(pageNumb);
-    };
-    statementShow(pageNumb);
-    options.forEach(option => {
-        buttonShow(option, pageNumb);
-    });
-    //opinionShow();
-    
-}
-function back(){
-    pageCount--;
-    results.splice (pageCount,pageCount);
-    buildStatement();
-    
-}
-
-function clicked(event){
-    var data = event.target.choice;
-    var pageNumb = event.target.numb;
-
-    if(pageNumb <  subjects.length - 1 ){
-        var antwoord = {statement:pageNumb, anwser:data};
-        results.push(antwoord);
-        pageCount ++;
-        buildStatement(pageNumb);      
+function buildStatement(){
+    //change page
+    pageNumb++
+    page.style.display = "none";   
+    statementPage.style.display = "block";
+    //show backbutton
+    if(pageNumb >= 1){
+        backButton.style.display = "block";
     }else{
-        buildSkipped();
+        backButton.style.display = "none";
+    };
+    //change text
+    titleText.innerHTML = subjects[pageNumb].title;
+    statementText.innerHTML = subjects[pageNumb].statement;
+}
+function buildResults(){
+    //change page
+    statementPage.style.display = "none";
+    resultsPage.style.display = "block";
+
+    //create button
+    var row = document.getElementById("statementResults");
+
+    results.forEach(result =>{
+        var button = document.createElement("button");
+        numb = result.statement + 1;
+        //create button and at to row
+        button.setAttribute("id", numb);
+        row.appendChild(button);
+        //change button text to statement number
+        but = document.getElementById(numb);
+        but.innerHTML = numb;
+        console.log("show");
+        console.dir(but);
+    });
+}
+
+function back(){
+    pageNumb = pageNumb - 2;
+    results.splice(pageNumb, 1);    
+    buildStatement();
+}
+
+function clicked(choice){
+    //save anwsers and show result page
+    if(pageNumb >  subjects.length ){
+        var antwoord = {statement:pageNumb, anwser:choice};
+        results.push(antwoord);
+        buildStatement();     
+    }else{
+       buildResults(); 
     }
-    console.log(pageNumb);
-    console.log(data);
-    
 
 }
--- pageConstructors.js
+++ pageConstructors.js
@@ -1,149 +1,119 @@
-function statementShow(pageNumb){
-     //text van statement plaatsen
-     var title = document.createElement("h1");
-     var titleNode = document.createTextNode(subjects[pageNumb].title);
-     title.appendChild(titleNode);
-     page.appendChild(title);
- 
-     var statement = document.createElement("p");
-     var statementNode = document.createTextNode(subjects[pageNumb].statement);
-     statement.appendChild(statementNode);
-     page.appendChild(statement);
- 
-}
 
-function resultPageShow(){
-    results.forEach
+// function resultPageShow(){
+//     results.forEach
 
-}
-function skippedShow(){
-    var i = 1;
-    pageNumb = 0;
-    results.forEach(result =>{
-        var resLink = document.createElement("p");
-        resLink.addEventListener('click', function (event) {
-            buildStatement(pageNumb);
-          });
-        resLink.style.cssText = "height: 20px; width: 20px; margin right: 10px;";        
-        var node = document.createTextNode(i);
-        i++;
-        pageNumb++;
-        console.log(result.anwser);
-        resLink.appendChild(node);
-        if(result.anwser == "skip"){
-            resLink.setAttribute("class", "w3-red");
-        }else{
-            resLink.setAttribute("class", "w3-green");   
-        }
-        page.appendChild(resLink);
-    });
+// }
+// function skippedShow(){
+//     var i = 1;
+//     pageNumb = 0;
+//     results.forEach(result =>{
+//         var resLink = document.createElement("p");
+//         resLink.addEventListener('click', function (event) {
+//             buildStatement(pageNumb);
+//           });
+//         resLink.style.cssText = "height: 20px; width: 20px; margin right: 10px;";        
+//         var node = document.createTextNode(i);
+//         i++;
+//         pageNumb++;
+//         console.log(result.anwser);
+//         resLink.appendChild(node);
+//         if(result.anwser == "skip"){
+//             resLink.setAttribute("class", "w3-red");
+//         }else{
+//             resLink.setAttribute("class", "w3-green");   
+//         }
+//         page.appendChild(resLink);
+//     });
 
-}
+// }
 
-function opinionShow(){
-    var div = document.createElement("div");
-    div.setAttribute("style", "display: flex; height: 400px; overflow: auto;" );
+// function opinionShow(){
+//     var div = document.createElement("div");
+//     div.setAttribute("style", "display: flex; height: 400px; overflow: auto;" );
 
-    page.appendChild(div);
+//     page.appendChild(div);
 
-    for(var i = 1; i < 4; i++){
-        var banaan = i-1;
-        var card = document.createElement("div");
-        card.setAttribute("class","w3-card-4 w3-light-grey w3-container")
-        card.setAttribute("id", "position".concat(i));
+//     for(var i = 1; i < 4; i++){
+//         var banaan = i-1;
+//         var card = document.createElement("div");
+//         card.setAttribute("class","w3-card-4 w3-light-grey w3-container")
+//         card.setAttribute("id", "position".concat(i));
 
-        var h1 = document.createElement("h3");
-        var h1Text = document.createTextNode(options[banaan]);        
-        h1.appendChild(h1Text);
+//         var h1 = document.createElement("h3");
+//         var h1Text = document.createTextNode(options[banaan]);        
+//         h1.appendChild(h1Text);
 
-        var table = document.createElement("table");
-        table.setAttribute("class", "w3-table-all")
-        var tr = document.createElement("tr");
+//         var table = document.createElement("table");
+//         table.setAttribute("class", "w3-table-all")
+//         var tr = document.createElement("tr");
 
-        var thName = document.createElement("th");
-        var node1 = document.createTextNode("naam");
-        thName.style.cssText = "padding-left: 10%";        
-        thName.appendChild(node1);
+//         var thName = document.createElement("th");
+//         var node1 = document.createTextNode("naam");
+//         thName.style.cssText = "padding-left: 10%";        
+//         thName.appendChild(node1);
 
-        var thPos = document.createElement("th");
-        var node2 = document.createTextNode("toelichting");
-        thPos.style.cssText = "pull: left; margin-left: 10%";
-        thPos.appendChild(node2);
+//         var thPos = document.createElement("th");
+//         var node2 = document.createTextNode("toelichting");
+//         thPos.style.cssText = "pull: left; margin-left: 10%";
+//         thPos.appendChild(node2);
         
-        tr.appendChild(thName);
-        tr.appendChild(thPos);
-        table.appendChild(tr);
-        card.appendChild(h1);
-        card.appendChild(table);
-        div.appendChild(card);
-    }
-    var card1 = document.getElementById("position1");
-    card1.style.cssText = "margin-right: 4%; width: 31%; height:100%; overflow: auto;";
+//         tr.appendChild(thName);
+//         tr.appendChild(thPos);
+//         table.appendChild(tr);
+//         card.appendChild(h1);
+//         card.appendChild(table);
+//         div.appendChild(card);
+//     }
+//     var card1 = document.getElementById("position1");
+//     card1.style.cssText = "margin-right: 4%; width: 31%; height:100%; overflow: auto;";
         
-    var card2 = document.getElementById("position2");
-    card2.style.cssText = "width: 31%; margin-right: 4%; height:100%; overflow: auto;";
+//     var card2 = document.getElementById("position2");
+//     card2.style.cssText = "width: 31%; margin-right: 4%; height:100%; overflow: auto;";
 
-    var card3 = document.getElementById("position3");
-    card3.style.cssText = "width: 31%; height:100%; overflow: auto;";
+//     var card3 = document.getElementById("position3");
+//     card3.style.cssText = "width: 31%; height:100%; overflow: auto;";
     
-    subjects[pageCount].parties.forEach(partie => {
-        var tr = document.createElement("th");
-        var th1 = document.createElement("th");
-        var text1 = partie.name;
-        var a = document.createTextNode(text1);
-        var th2 = document.createElement("th");
-        var text2 = partie.position;        
-        var b = document.createTextNode(text2);
-        if(b == "pro"){
-            b = "eens";
-            th1.appendChild(a);
-            th2.appendChild(b);
-            tr.appendChild(th1);
-            tr.appendChild(th2);
-            card1.appendChild(tr);
-        }else if(b == "contra"){
-            b = "oneens";
-            th1.appendChild(a);
-            th2.appendChild(b);
-            tr.appendChild(th1);
-            tr.appendChild(th2);
-            card3.appendChild(tr);
+//     subjects[pageCount].parties.forEach(partie => {
+//         var tr = document.createElement("th");
+//         var th1 = document.createElement("th");
+//         var text1 = partie.name;
+//         var a = document.createTextNode(text1);
+//         var th2 = document.createElement("th");
+//         var text2 = partie.position;        
+//         var b = document.createTextNode(text2);
+//         if(b == "pro"){
+//             b = "eens";
+//             th1.appendChild(a);
+//             th2.appendChild(b);
+//             tr.appendChild(th1);
+//             tr.appendChild(th2);
+//             card1.appendChild(tr);
+//         }else if(b == "contra"){
+//             b = "oneens";
+//             th1.appendChild(a);
+//             th2.appendChild(b);
+//             tr.appendChild(th1);
+//             tr.appendChild(th2);
+//             card3.appendChild(tr);
             
-        }else{
-            b = "geen van beide";
-            th1.appendChild(a);
-            th2.appendChild(b);
-            tr.appendChild(th1);
-            tr.appendChild(th2);
-            card2.appendChild(tr);
+//         }else{
+//             b = "geen van beide";
+//             th1.appendChild(a);
+//             th2.appendChild(b);
+//             tr.appendChild(th1);
+//             tr.appendChild(th2);
+//             card2.appendChild(tr);
             
-        }
-        console.log(b);
+//         }
+//         console.log(b);
                 
-    });
+//     });
  
 
-}
+// }
 
-
-//build the buttons
-function buttonShow(option, pageNumb){
-        var button = document.createElement("button");
-        var Node = document.createTextNode(option);
-        button.setAttribute("class", "w3-button w3-teal w3-margin");
-        button.onclick = clicked;
-        button.choice = option;
-        button.numb = pageNumb;
-        button.appendChild(Node);
-        page.appendChild(button);
-
-}
 
 function backButtonShow(pageCount){
-        var button = document.createElement("button");
-        var node = document.createTextNode("terug");
-        button.setAttribute("class", "w3-button w3-grey w3");
-        button.setAttribute("onclick", "back()");
-        button. appendChild(node);
-        page.appendChild(button);
+    backButton = document.getElementById('backButton');
+    backButton.style.display = "block";
 }
