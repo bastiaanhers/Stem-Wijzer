ebfbd5b03e73e52163debb3155332c3f2a99c971
bastiaan hersman <bastiaan.hers@gmail.com>
2018-02-13 16:02:09 +0100

    show results and better statement navigation

--- code.js
+++ code.js
@@ -8,25 +8,83 @@ var titleText = document.getElementById("titleText");
 var statementText = document.getElementById("statementText");
 //result page
 var resultsPage = document.getElementById("resultsPage");
+//button elements
+var button1 = document.getElementById("but1");
+var button2 = document.getElementById("but2");
+var button3 = document.getElementById("but3");
+var button4 = document.getElementById("but4");
+var granted = document.getElementById("granted");
+var blocked = document.getElementById("blocked");
 
 var results = [];
 
 
-function buildStatement(){
+function buildStatement(event){
+    // hide elements
+    resultsPage.style.display = "none";
+    blocked.style.display = "none";
+    granted.style.display = "none";
+    
+    //add to change to next page
+    pageNumb++;
+    //if result button is clicked load the right page
+    if(event){
+        number = event.target.id - 1;
+        pageNumb = number;
+    }
+   
     //change page
-    pageNumb++
     page.style.display = "none";   
     statementPage.style.display = "block";
+
     //show backbutton
     if(pageNumb >= 1){
         backButton.style.display = "block";
     }else{
         backButton.style.display = "none";
     };
+
     //change text
     titleText.innerHTML = subjects[pageNumb].title;
     statementText.innerHTML = subjects[pageNumb].statement;
+    //set color back to basic
+    button1.setAttribute("class", "w3-button w3-teal w3-margin");
+    button2.setAttribute("class", "w3-button w3-teal w3-margin");
+    button3.setAttribute("class", "w3-button w3-teal w3-margin");
+    button4.setAttribute("class", "w3-button w3-teal w3-margin");
+    //change color if already selected
+    if(results[pageNumb]){
+        switch(results[pageNumb].anwser) {
+            case "eens":
+                button1.setAttribute("class", "w3-button w3-green w3-margin");
+                console.log("eens");
+            break;
+            case "geenVanBeide":
+                button2.setAttribute("class", "w3-button w3-green w3-margin");
+                console.log("geen van beide");
+                break;
+            case "oneens":
+                button3.setAttribute("class", "w3-button w3-green w3-margin");
+            break;
+            case "skip":
+                button4.setAttribute("class", "w3-button w3-red w3-margin");
+            break;
+        }
+        console.log(pageNumb);
+    }
+    //if all the anwsers are given show button to result page;
+    if(results.length >= subjects.length){
+        var result = results[pageNumb].anwser;
+        if(result == "skip"){
+            blocked.style.display = "block";
+        }else{
+            granted.style.display = "block";
+        }
+        console.dir(result);
+    }
 }
+
+
 function buildResults(){
     //change page
     statementPage.style.display = "none";
@@ -34,35 +92,52 @@ function buildResults(){
 
     //create button
     var row = document.getElementById("statementResults");
-
+    row.innerHTML = "";
     results.forEach(result =>{
         var button = document.createElement("button");
         numb = result.statement + 1;
+
         //create button and at to row
+        button.addEventListener("click", buildStatement);
         button.setAttribute("id", numb);
+
+        if(result.anwser == "skip"){
+            button.setAttribute("class", "w3-red");            
+        }else{
+            button.setAttribute("class", "w3-green");
+        }
         row.appendChild(button);
         //change button text to statement number
         but = document.getElementById(numb);
         but.innerHTML = numb;
-        console.log("show");
-        console.dir(but);
     });
 }
 
+
 function back(){
     pageNumb = pageNumb - 2;
-    results.splice(pageNumb, 1);    
     buildStatement();
 }
 
 function clicked(choice){
-    //save anwsers and show result page
-    if(pageNumb >  subjects.length ){
+        //delete result if exists
+        if(results[pageNumb]){
+            results.splice(pageNumb, 1); 
+            results.sort(function(a, b){return a.statement - b.statement});                                           
+        }
+
+        //save given result
         var antwoord = {statement:pageNumb, anwser:choice};
         results.push(antwoord);
-        buildStatement();     
-    }else{
-       buildResults(); 
-    }
+        results.sort(function(a, b){return a.statement - b.statement});
+        
 
+    // show result if all anwers are given else show next statement
+    console.log(subjects);
+     if(pageNumb >= subjects.length -1){
+        results.sort(function(a, b){return a.statement - b.statement});
+        buildResults(); 
+    }else{
+        buildStatement();
+    }
 }
